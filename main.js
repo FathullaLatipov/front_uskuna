6ef6cabc95f262dcf7704151a7f97c877c46aeb0
apple <apple@iMac-apple.local>
2022-01-16 23:27:59 -0800

    products1

--- main.js
+++ main.js
@@ -27,3 +27,5 @@ dropdownArray.forEach(function(el){
 Element.prototype.hasClass = function(className) {
     return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 };
+
+
